3c6f175f80187ab89d0313cac00d99836a0ab959
khiba-k <khibakoenane@gmail.com>
2026-01-12 17:51:49 +0200

    chore: resolve merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
->>>>>>> fc064152cbf5d6df436ff94653a895e74e9df2d1
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,8 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-<<<<<<< HEAD
 })
-=======
-});
->>>>>>> fc064152cbf5d6df436ff94653a895e74e9df2d1
